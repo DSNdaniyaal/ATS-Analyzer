efbba8a91210c058cd34c6e4cc08581d9ffdcb77
DANIYAAL <daniyaalnayeem78653@gmail.com>
2026-04-08 23:04:22 +0530

    Add authentication using puter

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/">
-      <p> ResumeKnight </p></Link>
+      <p className="text-2xl font-bold text-gradient"> ResumeKnight </p></Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume</Link>
     </nav>
   )
 }
